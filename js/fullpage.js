90c369279cef54b90fbc05bde0491ebfe0fd47ab
androbaut <Androbaut@andersons-imac.attlocal.net>
2016-10-24 12:30:01 -0700

    instagram hover

--- js/fullpage.js
+++ js/fullpage.js
@@ -9,3 +9,7 @@ $(window).scroll(function(){
         var scroll = $(window).scrollTop();
             $('#footer').css({'opacity':(( 60-scroll )/60)});
         });
+
+$(document).ready(function(){
+  $('#page-top').fadeIn(500);
+});
